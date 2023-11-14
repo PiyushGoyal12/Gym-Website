24684b5b7e4a1890ebb9f07d86db69bd36740db1
Piyush Goyal <2004piyushgoyal@gmail.com>
2023-11-14 16:47:33 +0530

    made it responsive for phone and laptop not tabs

---
+++ new.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("links-list");
+    if (x.style.display === "grid") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "grid";
+    }
+}
